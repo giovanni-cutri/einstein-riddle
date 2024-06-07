74e37311b1a4577a99fec290d4daa2dd784e79ee
Giovanni Cutrì <giovannicutri02@gmail.com>
2024-06-07 16:41:05 +0200

    Add dark mode

--- scripts/script.js
+++ scripts/script.js
@@ -192,3 +192,16 @@ function submit() {
 }
 
 document.getElementsByTagName("button")[0].addEventListener("click", submit);
+
+
+function darkMode() {
+  dark_mode_switch = document.getElementsByTagName("html")[0];
+
+  if (dark_mode_switch.hasAttribute("data-bs-theme")) {
+    dark_mode_switch.removeAttribute("data-bs-theme")
+  } else {
+    dark_mode_switch.setAttribute("data-bs-theme", "dark");
+  }
+}
+
+document.getElementById("dark-mode-switch").addEventListener("click", darkMode)
